aa2dc8ac62cf0439f5421173321555c56b13d6f9
marty-cz <martin.brazdil@gmail.com>
2013-07-26 16:33:54 +0200

    Project created

---
+++ Basket/src/main/Basket/WeekListActivity.java
@@ -0,0 +1,11 @@
+package main.Basket;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/** Created by martin on 26.7.13. */
+public class WeekListActivity extends Activity {
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}
