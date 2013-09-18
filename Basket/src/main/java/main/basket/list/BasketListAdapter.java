3cb4e4eebba1ba3c373209d9b8b763b89ba6f002
Martin Brázdil <martin.brazdil@gmail.com>
2013-09-18 16:06:30 +0200

    Solved duplicate run icons

--- Basket/src/main/java/main/basket/list/BasketListAdapter.java
+++ Basket/src/main/java/main/basket/list/BasketListAdapter.java
@@ -5,6 +5,7 @@ import android.content.res.Resources;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -12,41 +13,48 @@ import java.util.ArrayList;
 import main.basket.R;
 import main.basket.list.structure.BasketListItem;
 
-/** Created by martin on 27.7.13. */
+/**
+ * Created by martin on 27.7.13.
+ */
 public class BasketListAdapter extends BaseListAdapter {
 
-  public BasketListAdapter(Context context, ArrayList<BasketListItem> listData) {
-    super(context, listData);
-  }
-
-  public View getView(int position, View convertView, ViewGroup parent) {
-    ViewHolder holder;
-    if (convertView == null) {
-      convertView = layoutInflater.inflate(R.layout.basket_row_list_view_layout, null);
-      holder = new ViewHolder();
-      holder.headlineView  = (TextView) convertView.findViewById(R.id.title);
-      holder.priceView = (TextView) convertView.findViewById(R.id.price);
-      holder.buyedCheckBox = (CheckBox) convertView.findViewById(R.id.buyed);
-      convertView.setTag(holder);
-    } else {
-      holder = (ViewHolder) convertView.getTag();
+    public BasketListAdapter(Context context, ArrayList<BasketListItem> listData) {
+        super(context, listData);
     }
 
-    BasketListItem item = (BasketListItem) listData.get(position);
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = layoutInflater.inflate(R.layout.basket_row_list_view_layout, null);
+            holder = new ViewHolder();
+            holder.headlineView = (TextView) convertView.findViewById(R.id.title);
+            holder.priceView = (TextView) convertView.findViewById(R.id.price);
+            holder.buyedCheckBox = (CheckBox) convertView.findViewById(R.id.buyed);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
 
-    Resources res = layoutInflater.getContext().getResources();
+        final BasketListItem item = (BasketListItem) listData.get(position);
 
-    holder.headlineView.setText(item.getHeadLine());
-    holder.priceView.setText(String.format("%4.2f ",item.getPrice()) + context.getString(R.string.price_unit));
-    holder.buyedCheckBox.setChecked(item.isBuyed());
+        Resources res = layoutInflater.getContext().getResources();
 
-    return convertView;
-  }
+        holder.headlineView.setText(item.getHeadLine());
+        holder.priceView.setText(String.format("%4.2f ", item.getPrice()) + context.getString(R.string.price_unit));
+        holder.buyedCheckBox.setChecked(item.isBuyed());
+        holder.buyedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                item.setBuyed(isChecked);
+            }
+        });
 
-  static class ViewHolder {
-    TextView headlineView;
-    TextView priceView;
-    CheckBox buyedCheckBox;
-  }
+        return convertView;
+    }
+
+    static class ViewHolder {
+        TextView headlineView;
+        TextView priceView;
+        CheckBox buyedCheckBox;
+    }
 
 }
